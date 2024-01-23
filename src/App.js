3a1d22a258ad5a1261f1f4dabf88ff5457575342
md asfak <skmdashfakurrahman@gmail.com>
2024-01-23 22:48:23 +0530

    Evulation 5

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Todo from "./ToduList/Todo";
+
+export default function App() {
+  return (
+    <>
+      <center>
+        <h1>This Is Evulation 5</h1>
+      </center>
+
+      <Todo />
+    </>
+  );
+}
